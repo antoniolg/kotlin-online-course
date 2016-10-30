8dfd3f36558521511791ca44fc7def1166c6c985
Antonio Leiva <antonioleivag@gmail.com>
2016-10-30 17:11:08 +0100

    Find TextView and change its text programmatically

--- app/src/main/java/com/antonioleiva/myplayer/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/myplayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.antonioleiva.myplayer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toast("Hello world!")
+
+        val message = findViewById<TextView>(R.id.message)
+        message.text = "Hello Kotlin!"
     }
 
     private fun toast(message: String) {
