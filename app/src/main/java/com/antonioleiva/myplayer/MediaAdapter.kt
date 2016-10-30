98a56643e3c01a4e770b1f3a62471f5c37a7ca5a
Antonio Leiva <antonioleivag@gmail.com>
2016-10-30 18:59:01 +0100

    Properties creation for MediaAdapter

--- app/src/main/java/com/antonioleiva/myplayer/MediaAdapter.kt
+++ app/src/main/java/com/antonioleiva/myplayer/MediaAdapter.kt
@@ -4,14 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
-class MediaAdapter(data: List<MediaItem>) : RecyclerView.Adapter<MediaAdapter.ViewHolder>() {
-
-    private val data: List<MediaItem>
-
-    init {
-        this.data = data
-    }
+class MediaAdapter(private val data: List<MediaItem>) :
+    RecyclerView.Adapter<MediaAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.view_media_item, parent, false)
@@ -25,6 +22,9 @@ class MediaAdapter(data: List<MediaItem>) : RecyclerView.Adapter<MediaAdapter.Vi
     override fun getItemCount(): Int = data.size
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val title: TextView = view.findViewById(R.id.media_title)
+        private val thumb: ImageView = view.findViewById(R.id.media_thumb)
 
         fun bind(item: MediaItem) {
 
