27111ae721f4eb817f368d5489b581b92a0e268b
Antonio Leiva <antonioleivag@gmail.com>
2016-11-01 16:16:36 +0100

    Collection operations and ranges

--- app/src/main/java/com/antonioleiva/myplayer/GetMedia.kt
+++ app/src/main/java/com/antonioleiva/myplayer/GetMedia.kt
@@ -1,16 +1,11 @@
 package com.antonioleiva.myplayer
 
-private const val thumbBase = "http://lorempixel.com/400/400/cats/"
+import com.antonioleiva.myplayer.MediaItem.Type
+import java.util.*
 
-fun getMedia() = listOf(
-        MediaItem("Title 1", "${thumbBase}1", MediaItem.Type.PHOTO),
-        MediaItem("Title 2", "${thumbBase}2", MediaItem.Type.PHOTO),
-        MediaItem("Title 3", "${thumbBase}3", MediaItem.Type.VIDEO),
-        MediaItem("Title 4", "${thumbBase}4", MediaItem.Type.PHOTO),
-        MediaItem("Title 5", "${thumbBase}5", MediaItem.Type.PHOTO),
-        MediaItem("Title 6", "${thumbBase}6", MediaItem.Type.VIDEO),
-        MediaItem("Title 7", "${thumbBase}7", MediaItem.Type.VIDEO),
-        MediaItem("Title 8", "${thumbBase}8", MediaItem.Type.PHOTO),
-        MediaItem("Title 9", "${thumbBase}9", MediaItem.Type.PHOTO),
-        MediaItem("Title 10", "${thumbBase}10", MediaItem.Type.VIDEO)
-)
+private const val thumbBase = "http://lorempixel.com/400/400/cats/"
+private val rnd = Random(1)
+
+fun getMedia() = (1..10).map { MediaItem("Title $it", "$thumbBase$it", randomType()) }
+
+private fun randomType() = rnd.nextInt(2).let { if(it == 0) Type.PHOTO else Type.VIDEO }
--- app/src/main/java/com/antonioleiva/myplayer/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/myplayer/MainActivity.kt
@@ -2,6 +2,8 @@ package com.antonioleiva.myplayer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recycler.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        adapter.data = getMedia().let { media ->
+            when (item.itemId) {
+                R.id.filter_all -> media
+                R.id.filter_photos -> media.filter { it.type == MediaItem.Type.PHOTO }
+                R.id.filter_videos -> media.filter { it.type == MediaItem.Type.VIDEO }
+                else -> emptyList()
+            }
+        }
+
+        return true
     }
 }
