761b061857eeed7f9872959172e1007355e74f0e
Antonio Leiva <antonioleivag@gmail.com>
2016-11-03 09:07:17 +0100

    Create some tests

---
+++ app/src/test/java/com/antonioleiva/myplayer/MainPresenterTest.kt
@@ -0,0 +1,47 @@
+package com.antonioleiva.myplayer
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.verify
+
+class MainPresenterTest {
+
+    private val view = mock<MainPresenter.View>()
+    private val provider = mock<Provider>()
+    private lateinit var presenter: MainPresenter
+
+    @Before
+    fun setUp() {
+        presenter = MainPresenter(view, provider)
+    }
+
+    @Test
+    fun providerIsCalledInOnCreate() {
+        presenter.onCreate()
+        verify(provider).dataAsync(any())
+    }
+
+    @Test
+    fun progressIsShownInOnCreate() {
+        presenter.onCreate()
+        verify(view).showProgress()
+    }
+
+    @Test
+    fun uiIsUpdatedWithResults() {
+        presenter.onCreate()
+        val media = mock<List<MediaItem>>()
+
+        argumentCaptor<(List<MediaItem>) -> Unit>().apply {
+            verify(provider).dataAsync(capture())
+            val f = allValues[0]
+            f(media)
+
+            verify(view).updateData(media)
+        }
+    }
+
+}
