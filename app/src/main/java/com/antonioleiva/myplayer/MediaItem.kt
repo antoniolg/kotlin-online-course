5b0bbbedf67013558c10eaf7312b6434dd0838ed
Antonio Leiva <antonioleivag@gmail.com>
2020-03-13 15:57:38 +0100

    Use of Enum and When
    (cherry picked from commit b5b295eeb48e2642bf99230613733142bfdbd7a4)
    (cherry picked from commit ebb91fe60513b607ed5bf89b53511cfe689b003d)

--- app/src/main/java/com/antonioleiva/myplayer/MediaItem.kt
+++ app/src/main/java/com/antonioleiva/myplayer/MediaItem.kt
@@ -1,3 +1,5 @@
 package com.antonioleiva.myplayer
 
-data class MediaItem(val title: String, val url: String)
+data class MediaItem(val title: String, val url: String, val type: Type) {
+    enum class Type { PHOTO, VIDEO }
+}
