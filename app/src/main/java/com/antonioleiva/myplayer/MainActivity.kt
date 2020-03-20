fc68ae2d44ce0944385917bdfd6a87ce628646ca
Antonio Leiva <antonioleivag@gmail.com>
2020-03-20 09:08:59 +0100

    Set a listener to the adapter using a lambda
    (cherry picked from commit a3c19fc01c77cada851199b0da318f6360f966e6)
    (cherry picked from commit 5bd436c37b3b34da5fea258c0a48555713624ff0)

--- app/src/main/java/com/antonioleiva/myplayer/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/myplayer/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.recycler.adapter = MediaAdapter(getItems())
+        binding.recycler.adapter = MediaAdapter(getItems()) {
+            toast(it.title)
+        }
     }
 }
