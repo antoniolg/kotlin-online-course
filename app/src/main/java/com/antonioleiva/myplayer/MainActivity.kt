87036c0c5f2ae661ac80a58a49c5c5bde03241df
Antonio Leiva <antonioleivag@gmail.com>
2020-03-20 12:22:26 +0100

    Collection operations and ranges
    (cherry picked from commit ef9ae1f6424cc91347fe882364d4f851a02d120a)
    (cherry picked from commit 3b5e5a663fb3dc362ffe1490da04daeddfbae785)

--- app/src/main/java/com/antonioleiva/myplayer/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/myplayer/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.antonioleiva.myplayer
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.antonioleiva.myplayer.databinding.ActivityMainBinding
@@ -15,5 +17,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.recycler.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        adapter.items = getItems().let { media ->
+            when (item.itemId) {
+                R.id.filter_all -> media
+                R.id.filter_photos -> media.filter { it.type == MediaItem.Type.PHOTO }
+                R.id.filter_videos -> media.filter { it.type == MediaItem.Type.VIDEO }
+                else -> emptyList()
+            }
+        }
+
+        return true
     }
 }
