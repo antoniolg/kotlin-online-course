ef9ae1f6424cc91347fe882364d4f851a02d120a
Antonio Leiva <antonioleivag@gmail.com>
2020-03-20 12:22:26 +0100

    Collection operations and ranges

--- app/src/main/java/com/antonioleiva/myplayer/getItems.kt
+++ app/src/main/java/com/antonioleiva/myplayer/getItems.kt
@@ -1,14 +1,11 @@
 package com.antonioleiva.myplayer
 
-fun getItems(): List<MediaItem> = listOf(
-    MediaItem("Title 1", "https://placekitten.com/200/200?image=1", MediaItem.Type.PHOTO),
-    MediaItem("Title 2", "https://placekitten.com/200/200?image=2", MediaItem.Type.PHOTO),
-    MediaItem("Title 3", "https://placekitten.com/200/200?image=3", MediaItem.Type.VIDEO),
-    MediaItem("Title 4", "https://placekitten.com/200/200?image=4", MediaItem.Type.PHOTO),
-    MediaItem("Title 5", "https://placekitten.com/200/200?image=5", MediaItem.Type.PHOTO),
-    MediaItem("Title 6", "https://placekitten.com/200/200?image=6", MediaItem.Type.VIDEO),
-    MediaItem("Title 7", "https://placekitten.com/200/200?image=7", MediaItem.Type.PHOTO),
-    MediaItem("Title 8", "https://placekitten.com/200/200?image=8", MediaItem.Type.PHOTO),
-    MediaItem("Title 9", "https://placekitten.com/200/200?image=9", MediaItem.Type.PHOTO),
-    MediaItem("Title 10", "https://placekitten.com/200/200?image=10", MediaItem.Type.VIDEO)
-)
+import com.antonioleiva.myplayer.MediaItem.Type
+
+fun getItems() = (1..10).map {
+    MediaItem(
+        "Title $it",
+        "https://placekitten.com/200/200?image=$it",
+        if (it % 3 == 0) Type.VIDEO else Type.PHOTO
+    )
+}
